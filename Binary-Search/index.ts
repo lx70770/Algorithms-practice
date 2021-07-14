b793ea02a28669e8f9d8da540894d047d974e8bf
李翔 <lixiang16@corp.netease.com>
2021-07-14 14:35:10 +0800

    feat: binary search

--- Binary-Search/index.ts
+++ Binary-Search/index.ts
@@ -1,4 +1,5 @@
-export const BinarySearch = (arr, target) => {
+// 二分查找递归写法
+export const BinarySearchR = (arr, target) => {
 	function search(data, l, r, t) {
 		if (l > r) return -1
 
@@ -9,6 +10,25 @@ export const BinarySearch = (arr, target) => {
 		if (target > data[mid]) return search(data, mid + 1, r, t)
 
 		if (target < data[mid]) return search(data, l, mid - 1, t)
+	}
+
+	return search(arr, 0, arr.length - 1, target)
+}
+
+// 非递归实现
+export const BinarySearch = (arr, target) => {
+	function search(data, l, r, t) {
+		while (l <= r) {
+			let mid = Math.floor(l + (r - l) / 2)
+
+			if (data[mid] == t) return mid
+
+			if (t > data[mid]) l++
+
+			if (t < data[mid]) r--
+		}
+
+		return -1
 	}
 
 	return search(arr, 0, arr.length - 1, target)
